4acd9284817d39d15e3b99715c17c06d11113133
Murillo de Morais <mm@in3d.com.br>
2020-07-09 14:55:51 -0300

    feat: implements contact in topbar

--- components/TopBar/index.js
+++ components/TopBar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import { FiMail } from 'react-icons/fi';
 
 const TopBar = ({ products }) => (
   <section id="top-bar">
@@ -17,12 +18,34 @@ const TopBar = ({ products }) => (
           </Link>
         ))
       }
+      <a className="contact-mail" href="mailto:info@4dj.com.br"><FiMail /><span>info@4dj.com.br</span></a>
     </ul>
     <style>{`     
       #top-bar {
         display: flex;
         justify-content: space-between;
       } 
+      .contact-mail {
+        align-items: center;
+        background: rgba(255,255,255,.05);
+        border: 1px solid rgba(255,255,255,.2);
+        border-radius: 10px;
+        color: #fff;
+        display: flex;
+        margin-left: 3vw;
+        font-weight: 600;
+        opacity: .75;
+        outline: none;
+        padding: 5px 10px;
+        text-decoration: none;
+        transition: .25s all ease;
+      }
+      .contact-mail:hover {
+        opacity: 1;
+      }
+      .contact-mail span {
+        margin-left: 10px;
+      }
       #main-logo {
         cursor: pointer;
         width: 150px;
