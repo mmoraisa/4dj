a2b1123ee5605fd995048b7963eb07103704c07d
Murillo de Morais <mm@in3d.com.br>
2020-07-08 23:56:37 -0300

    feat: implements async pre render functions

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -12,22 +12,24 @@ const Product = ({ slug }) => {
   )
 }
 
-Product.getInitialProps = async ({ query }) => {
-  const { slug } = query;
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      slug: params.slug
+    }
+  }
+}
+
+export async function getStaticPaths() {
 
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
   });
 
-  const entries = await client.getEntries({
-    content_type: 'product',
-    'fields.slug[in]': slug,
-  });
-
   return {
-    slug,
-    product: entries.items[0]
+    paths: ['/product/bag','/product/headphone-bag'],
+    fallback: true
   };
 }
 
